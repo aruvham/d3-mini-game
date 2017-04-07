2fe80bad3568163d1c240a936a20bc9e3b4c08ae
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-04-07 17:38:41 -0400

    fix collisions, broke render

--- game.js
+++ game.js
@@ -3,11 +3,35 @@
 var svg = d3.select("svg"),
     w = +svg.attr("width"),
     h = +svg.attr("height"),
-    radius = 32;
+    size = 30,
+    radius = radius,
+    rows = h / size,
+    cols = w / size;
 
 /* Game Setup */
 
-svg.style('background', 'black');
+// create grid data
+var cells = d3.range(0, rows * cols).map(d => {
+  var col = d % cols;
+  var row = (d - col) / cols;
+  return {
+    r: row,
+    c: col,
+    x: col * size,
+    y: row * size
+  };
+});
+
+// render grid
+var cell = svg.selectAll(".cell")
+  .data(cells)
+  .enter().append("rect")
+  .attr("x", (d) => d.x)
+  .attr("y", (d) => d.y)
+  .attr("width", size)
+  .attr("height", size)
+  .style("fill", "#333333")
+  .style("stroke", "#cccccc");
 
 /* Game Class */
 
@@ -15,23 +39,27 @@ Game = {
   fps: 60,
   fCounter: 0,
   circles: [],
-  nBalls: 2,
+  nBalls: 1,
 
   init: () => {
     for(var i = 0; i < Game.nBalls; i++) {
-      var randomPosX = Math.floor(Math.random() * w);
-      var randomPosY = Math.floor(Math.random() * h);
-      //var randomVelX = Math.floor(Math.random() * w);
-      //var randomVelY = Math.floor(Math.random() * w);
-
-      Game.circles.push(new Circle(randomPosX, randomPosY));
+      var y    = Math.floor(Math.random() * (h - 2 * size)) + size;
+      var x    = Math.floor(Math.random() * (w - 2 * size)) + size;
+      var v    = Math.floor(Math.random() * 5) + 5;
+      var teta = Math.floor(Math.random() * 360);
+      Game.circles.push(new Circle(
+        x,
+        y,
+        v * Math.sin(teta),
+        v * Math.cos(teta)
+      ));
     }
     Game.createCircles();
     Game._interval = setInterval(Game.update, 1000/Game.fps);
   },
 
   pause: () => {
-
+    clearInterval(Game._interval);
   },
 
   stop: () => {
@@ -40,14 +68,15 @@ Game = {
 
   update: () => {
     Game.fCounter++;
-    console.log(Game.fCounter);
+    d3.select(".frameCounter").text("Frame " + Game.fCounter);
 
     Game.updateCircles();
     Game.renderCircles();
   },
 
   updateCircles: () => {
-    Game.circles.forEach((circle) => circle.update());
+    //debugger;
+    Game.circles.forEach((circle) => {circle.update()}
   },
 
   renderCircles: () => {
@@ -55,6 +84,7 @@ Game = {
       .data(Game.circles)
       .attr("cx", (d) => d.pos.x)
       .attr("cy", (d) => d.pos.y);
+      //.style("fill", (d) => d.color);
   },
 
   createCircles: () => {
@@ -70,32 +100,43 @@ Game = {
 
 /* Circle Class */
 
-var Circle = function(x, y) {
+var Circle = function(x, y, dx, dy) {
   this.pos = { x: x, y: y };
-  this.vel = { x: 5, y: 5 };
-  this.color = 'red';
+  this.vel = { x: dx, y: dy };
+  this.color = /*randomColor();*/"red";
 }
 
-Circle.prototype.update = function() {
-  this.pos.x += this.vel.x;
-  this.pos.y += this.vel.y;
-
+Circle.prototype.update = () => {
   // collision detection with wall
-  if(this.pos.x + radius > w || this.pos.x - radius < 0) {
+  /*if(this.pos.x + this.vel.x > w - (radius) || this.pos.x + this.vel.x < (radius)) {
     this.vel.x *= -1;
-  }
-  if(this.pos.y + radius > h || this.pos.y - radius < 0) {
+    //this.color = randomColor();
+  } else*/
+  /*if(this.pos.y + this.vel.y > h - (radius) || this.pos.y + this.vel.y < (radius)) {
     this.vel.y *= -1;
-  }
-
-  // collision detection with other balls
-
+    //this.color = randomColor();
+  } else {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }*/
+  console.log(this)
 }
 
 /* Helper functions */
 
-var dist = function(x1, y1, x2, y2) {
+var dist = (x1, y1, x2, y2) => {
   var a = x1 - x2
   var b = y1 - y2
   return Math.sqrt(a * a + b * b);
+}
+
+var random = (min, max) => {
+  return Math.floor(Math.random() * min) + max;
+}
+
+var randomColor = () => {
+  var g = random(0, 255);
+  var r = random(0, 255);
+  var b = random(0, 255);
+  return `rgb({g},{r},{b})`;
 }
